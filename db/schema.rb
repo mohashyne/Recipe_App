af6fdb11c8edece590b007c53b3f58d796accb32
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-17 12:34:20 +0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_191914) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_093211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
