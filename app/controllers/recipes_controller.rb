7ae83b0ba7e945f75dbea30f67b10cada6aea30d
MSA <crystalbluetech@gmail.com>
2023-10-17 23:10:30 +0100

    fix errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,13 @@
 class RecipesController < ApplicationController
-#     # Devise for authentication
-#     before_action :authenticate_user! 
+  #     # Devise for authentication
+  #     before_action :authenticate_user!
 
-# # association between User and Recipe models
-#   def index
-#     @recipes = current_user.recipes 
-#   end
+  # # association between User and Recipe models
+  #   def index
+  #     @recipes = current_user.recipes
+  #   end
 
-
-def index
+  def index
     @recipes = Recipe.all
-end
-
-
+  end
 end
