f7230ed54f2a9f6bc259eebd3a7b2825c4adc53a
MSA <crystalbluetech@gmail.com>
2023-10-17 22:44:38 +0100

    implement view and setup controllers for the recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/foods', to: 'foods#index'
+
+  get '/recipes', to: 'recipes#index'
 
 end
