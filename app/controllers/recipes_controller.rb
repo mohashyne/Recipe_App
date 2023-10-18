0c89e17d5fec4c4c99a2969bcaaac71f28868b3e
MSA <crystalbluetech@gmail.com>
2023-10-18 18:56:02 +0100

    define  class method for public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,16 @@ class RecipesController < ApplicationController
   #     # Devise for authentication
   #     before_action :authenticate_user!
 
+  #   TODO:
   # # association between User and Recipe models
   #   def index
   #     @recipes = current_user.recipes
   #   end
-  def index
-    @inventories = current_user.inventories
-  end
+
+  #   TODO:
+  #   def index
+  #     @inventories = current_user.inventories
+  #   end
 
   def index
     @recipes = Recipe.ispublic
