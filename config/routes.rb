d9b674108eb156673f1e247f319919860b324cca
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-18 20:27:39 +0300

    Add route to recipes details

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/foods', to: 'foods#index'
+  
   get '/recipes', to: 'recipes#index', as: 'recipes'
   get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  resources :recipes, only: [:index, :show] do
+    patch 'update_status', on: :member
+  end
 
 end
