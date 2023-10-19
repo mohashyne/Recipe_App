c52ff33b58271bbfcec56ab7e91781ac531a7833
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-19 11:32:37 +0300

    Add the neccesary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_160116) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_082810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_160116) do
     t.string "name"
     t.string "measurement_unit"
     t.integer "price"
+    t.string "unit"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -37,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_160116) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.integer "food_id"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
