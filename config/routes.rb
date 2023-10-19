2b37573fc4f64d9079f70a6ef4f3edaddb426c19
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-19 17:53:16 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
 
 
   get '/foods', to: 'foods#index'
-  #get '/inventories/:id', to: 'inventories#index', as: 'inventories'
-  get '/inventories', to: 'inventories#index', as: 'inventories'
-
+  # get '/inventories/:id', to: 'inventories#index', as: 'inventories'
+  # get '/inventories', to: 'inventories#index', as: 'inventories'
 
   resources :recipes, only: [:index, :show] do
     patch 'update_status', on: :member
   end
 
+
   get '/public', to: 'recipes#public'
 
 
-  resources :inventories, only: [:index, :new, :create, :destroy]
+  # resources :inventories, only: [:index, :new, :create, :destroy]
+  resources :inventories, only: [:index, :new, :create, :destroy] 
 end
