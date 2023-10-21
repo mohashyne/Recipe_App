47ea838b7a9f6370693062f4d1c1aa6ea0fea76a
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-21 16:56:12 +0300

    Add a sign up page that redirects to sign up page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,6 +308,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :registerable
   config.sign_in_after_reset_password = :false
+  
 
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
